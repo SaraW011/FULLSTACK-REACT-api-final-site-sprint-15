e739e7eebcbd549cbd3d69adb3b0e404ae7b4947
SaraW011 <sarawsmn@gmail.com>
2022-07-31 20:03:28 +0300

    build

---
+++ frontend/build/precache-manifest.243205959381452873a36bbaa6b90f00.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab824b11f6000c11bbc5",
+    "url": "/static/css/main.7b12a372.chunk.css"
+  },
+  {
+    "revision": "ab824b11f6000c11bbc5",
+    "url": "/static/js/main.ab824b11.chunk.js"
+  },
+  {
+    "revision": "7ef873392d73c34b4b72",
+    "url": "/static/js/1.7ef87339.chunk.js"
+  },
+  {
+    "revision": "8a3083687ebde650b317",
+    "url": "/static/js/2.8a308368.chunk.js"
+  },
+  {
+    "revision": "9c1e9fa0fb3e25a65808",
+    "url": "/static/js/runtime~main.9c1e9fa0.js"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/static/media/logo.8f7611ae.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/static/media/vSign.e38b395c.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/static/media/xSign.4072126c.svg"
+  },
+  {
+    "revision": "9293aa8fd18af383d6eb4eeeecab483f",
+    "url": "/static/media/close_icon.9293aa8f.svg"
+  },
+  {
+    "revision": "b05083e51f8e21ef50c2b78b29bb740c",
+    "url": "/static/media/edit.b05083e5.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/static/media/pencil-edit-icon.d476a8b8.svg"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/static/media/plus.b2fce4ac.svg"
+  },
+  {
+    "revision": "7ee866f113eb2ed4216d588f5b956e6e",
+    "url": "/static/media/trash.7ee866f1.svg"
+  },
+  {
+    "revision": "ef38b085db5a81756d4dc5a64c1fccb8",
+    "url": "/static/media/heart_active.ef38b085.svg"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "dd31dd063f1c376deb28b582c4a64833",
+    "url": "/static/media/heart.dd31dd06.svg"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "a81dda1362e6a6945c0f9208e30608da",
+    "url": "/index.html"
+  }
+];
